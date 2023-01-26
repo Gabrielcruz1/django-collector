7cbad2022a34d133ceb2ab6cb05725baefe6ff8b
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-25 17:33:52 -0800

    Extended template view. Home Class working in browser

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
-from django.views import View #VIEW CLASS HANDLES REQUESTS
+
 from django.http import HttpResponse #CLASS HANDLES SENDING A TYPE OF RESPONSE
+
+from django.views.generic.base import TemplateView
+
 
 
 # Create your views here.
 
-#  class Home - extending it from View class
-class Home(View):
-
-    # method that will be ran when dealing with a GET request
-    def get(self, request):
-        # returning a generic response
-        # This is similar to response.send() in express
-        return HttpResponse("Band Collecter Home")
+#UPDATED HOME CLASS TO EXTEND TEMPLATEVIEW
+class Home(TemplateView):
+    template_name = "home.html"
 
 
-class About(View):
+# class About(View):
 
-    def get(self, request):
-        return HttpResponse("Band Collector About")
+#     def get(self, request):
+#         return HttpResponse("Band Collector About")
