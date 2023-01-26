949dff93103881e6db23012cd21f13932f44b6bb
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-25 17:23:16 -0800

    Basic about route added & working

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 # ADD OUR ROUTES BELOW
 # SIMILAR TO APP.USE() IN EXPRESS 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home")
+    path('', views.Home.as_view(), name="home"),
+    path('about/',views.About.as_view(), name="about")
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,11 +5,17 @@ from django.http import HttpResponse #CLASS HANDLES SENDING A TYPE OF RESPONSE
 
 # Create your views here.
 
-# Here we will be creating a class called Home and extending it from the View class
+#  class Home - extending it from View class
 class Home(View):
 
-    # Here we are adding a method that will be ran when we are dealing with a GET request
+    # method that will be ran when dealing with a GET request
     def get(self, request):
-        # Here we are returning a generic response
+        # returning a generic response
         # This is similar to response.send() in express
         return HttpResponse("Band Collecter Home")
+
+
+class About(View):
+
+    def get(self, request):
+        return HttpResponse("Band Collector About")
