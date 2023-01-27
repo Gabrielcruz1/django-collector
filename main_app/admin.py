6a323ecb09b71fa8d8fc5d696bd7c89be2548f8d
a.cruzgabriel98@gmail.com <steviebarrett@Stevies-MacBook-Air.local>
2023-01-26 16:20:41 -0800

    get band from data base working in broser, not displaying the bands

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Band # IMPORT BAND MODEL FROM MODEL.PY
+
 # Register your models here.
+admin.site.register(Band) # THIS ADDS BAND MODEL TO ADMIN PANNEL 
--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,7 @@ class Band(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=250)
     bio = models.TextField(max_length=500)
-    verified_artist = models.BooleanField(default=False)
+    verified_band = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
